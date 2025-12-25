4fbf522260201c7901cfaa2a23b70dc7a92e3dc7
Revanth Singothu <revanthsingothu@gmail.com>
2025-12-25 13:46:24 +0000

    fix: cors for frontend prod

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const cors = require('cors');
 
 // express app
 const app = express();
+
+// cors
+app.use(cors({
+  origin: "https://workout-tracker-frontend-zeta.vercel.app" 
+}));
 
 // middleware
 app.use(express.json());
